5b6660858d25af964006244c4290b56b25ad84db
98sunny <sunny.singh@cgi.com>
2022-07-31 03:00:28 -0400

    BeforeEach Demo

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -2,16 +2,21 @@ import { LoggerService } from "../Logger/logger.service";
 import { CalculatorService } from "./calculator.service";
 
 describe('CalculatorService',() => {
+  let mockLoggerService:any;
+  let calculator:CalculatorService;
+  beforeEach(() => {
+    console.log('In Before each');
+    mockLoggerService = jasmine.createSpyObj('LoggerService',['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  })
   it('should add two numbers',() => {
-    let mockLoggerService=jasmine.createSpyObj('LoggerService',['log']);
-    const calculator = new CalculatorService(mockLoggerService);
+    console.log("In the add test case");
     let result=calculator.add(1,2);
     expect(result).toBe(3);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract two numbers',() => {
-    let mockLoggerService=jasmine.createSpyObj('LogggerService',['log']);
-    const calculator = new CalculatorService(mockLoggerService);
+    console.log("In the subtract test case");
     let result=calculator.subtract(5,2);
     expect(result).toBe(3);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
