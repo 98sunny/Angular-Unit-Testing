25771f0c628731e9421ef736be43fa144c3c1d85
98sunny <sunny.singh@cgi.com>
2022-07-31 01:45:10 -0400

    Mocking injected Service

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -3,19 +3,17 @@ import { CalculatorService } from "./calculator.service";
 
 describe('CalculatorService',() => {
   it('should add two numbers',() => {
-    const loggerService=new LoggerService();
-    spyOn(loggerService,'log');
-    const calculator = new CalculatorService(loggerService);
+    let mockLoggerService=jasmine.createSpyObj('LoggerService',['log']);
+    const calculator = new CalculatorService(mockLoggerService);
     let result=calculator.add(1,2);
     expect(result).toBe(3);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract two numbers',() => {
-    const loggerService=new LoggerService();
-    spyOn(loggerService,'log');
-    const calculator = new CalculatorService(loggerService);
+    let mockLoggerService=jasmine.createSpyObj('LogggerService',['log']);
+    const calculator = new CalculatorService(mockLoggerService);
     let result=calculator.subtract(5,2);
     expect(result).toBe(3);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 })
